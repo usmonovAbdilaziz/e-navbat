9ff938bee8d82d12925ca22380cbaa8619c78a35
usmonovAbdilaziz <abdilazizusmonov993@gmail.com>
2025-06-25 12:16:40 +0500

    full crud  amaliyoti tugatildi

--- src/guards/roles.guard.js
+++ src/guards/roles.guard.js
@@ -1,6 +1,6 @@
 import { handleError } from "../helpers/error.js";
 
-export const C = (includeRoles = []) => {
+export const RolesGuard = (includeRoles = []) => {
   return (req, res, next) => {
     if (!includeRoles.includes(req.user?.role)) {
       return handleError(res, "Forbidden user", 403);
